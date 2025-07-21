fbd8ae0d4af7285d86a3089e06543f1370993962
everton-lourens <evertonlourens@gmail.com>
2025-07-21 15:49:49 -0300

    update readme

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,8 +2,8 @@ import { Tabs } from 'expo-router';
 import { Platform } from 'react-native';
 
 import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
+import { IconSymbol } from '@/components/_ui/IconSymbol';
+import TabBarBackground from '@/components/_ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
